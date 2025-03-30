2973889432e2c06808924da7e8ab906bafce6afc
Amit Khotele <142614018+amitkhotele@users.noreply.github.com>
2025-03-30 11:28:45 +0530

    Day 4- Rotate Array.py

---
+++ GfG 160 - 160 Days of Problem Solving/Day 4- Rotate Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        d %= n
+        arr[0:d] = reversed(arr[0:d])
+        arr[d:n] = reversed(arr[d:n])
+        arr[0:n] = reversed(arr[0:n])
