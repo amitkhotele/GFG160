a27ff33a05241e4f1ac8aee024cbce642ba65fe0
Amit Khotele <142614018+amitkhotele@users.noreply.github.com>
2025-04-11 16:22:58 +0530

    Day 16 - Anagram.py

---
+++ GfG 160 - 160 Days of Problem Solving/Day 16 - Anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def areAnagrams(self, s1, s2):
+        if len(s1) != len(s2):
+            return False
+
+        counts = [0] * 26
+        for i in range(len(s1)):
+            counts[ord(s1[i]) - ord('a')] += 1
+            counts[ord(s2[i]) - ord('a')] -= 1
+
+        for count in counts:
+            if count != 0:
+                return False
+
+        return True
