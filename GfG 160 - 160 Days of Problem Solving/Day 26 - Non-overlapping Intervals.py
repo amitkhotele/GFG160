a6e766a55783b715078359408d034329412b4f3d
Amit Khotele <142614018+amitkhotele@users.noreply.github.com>
2025-04-21 17:31:27 +0530

    Create Day 26 - Non-overlapping Intervals.py

---
+++ GfG 160 - 160 Days of Problem Solving/Day 26 - Non-overlapping Intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minRemoval(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        count, prevEnd = 0, intervals[0][1]
+
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < prevEnd:
+                count += 1
+                prevEnd = min(prevEnd, intervals[i][1])
+            else:
+                prevEnd = intervals[i][1]
+
+        return count
